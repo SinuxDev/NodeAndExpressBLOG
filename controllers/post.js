d7f80bab67dddec098b5ae47d18cacfe4fa4eb49
Aung Ye Htet <aung.atg1738@gmail.com>
2024-01-15 19:39:07 +0630

    AllFunctionsComplete

--- controllers/post.js
+++ controllers/post.js
@@ -5,15 +5,17 @@ const Posts = require("../models/posts");
 exports.createPost = (req, res) => {
   const { title, description, photo } = req.body;
 
-  const post = new Posts(title, description, photo);
-  post
-    .setPost()
-    .then(() => {
+  Posts.create({
+    title,
+    description,
+    imgUrl: photo,
+  })
+    .then((result) => {
+      console.log(result);
+      console.log("New Post created");
       res.redirect("/");
     })
     .catch((err) => console.log(err));
-
-  console.log(post);
 };
 
 exports.renderCreatePage = (req, res) => {
@@ -21,22 +23,24 @@ exports.renderCreatePage = (req, res) => {
 };
 
 exports.getPosts = (req, res) => {
-  // res.sendFile(path.join(__dirname, "..", "views", "homepage.html"));
-
-  Posts.getAllPost()
-    .then(([rows]) => {
-      console.log(rows);
-      res.render("home", { title: "Hello World", postsArr: rows });
+  Posts.findAll()
+    .then((post) => {
+      res.render("home", { title: "Home Page", postsArr: post });
     })
     .catch((err) => console.log(err));
 };
 
 exports.getPostDetails = (req, res) => {
   const postId = req.params.postId;
-  console.log(postId);
-  Posts.getSinglePost(postId)
-    .then(([row]) => {
-      res.render("details", { title: "I'm Details Page", post: row[0] });
+  Posts.findByPk(postId)
+    .then((post) => {
+      res.render("details", { title: "I'm Details Page", post });
     })
     .catch((err) => console.log(err));
+
+  // Posts.findOne({where : {id: postId}})
+  // .then((post) => {
+  //   res.render("details", { title: "I'm Details Page", post });
+  // })
+  // .catch((err) => console.log(err));
 };
--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,8 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize();
+const sequelize = new Sequelize("nodejsblog", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+module.exports = sequelize;
